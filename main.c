9f9e8b0ea4aa8830aa2f5e879a43ca5911890d0b
Imam Nur <imamnurfahudi@gmail.com>
2025-11-22 00:55:52 +0700

    chore: add format specifier

--- main.c
+++ main.c
@@ -3,87 +3,43 @@
 
 int main() {
   /**
-   *  variable = A reusable container for a value.
-   *             Behaves as if it were the value it contains.
-   *
-   *  int = whole numbers (4 bytes in modern systems)
-   *  float = single-precision decimal number (4 bytes)
-   *  double = double-precision decimal number (8 bytes)
-   *  char = single character (1 byte)
-   *  char[] = array of characters (size varies)
-   *  bool = true or false (1 byte, requires <stdbool.h>)
+   *  format specifier = Special tokens that begin with a % symbol,
+   *    followed by a character that specifies the data type
+   *    and optional modifiers (width, precision, flags).
+   *    They control how data is displayed or interpreted.
    */
 
-  int age = 25;
-  printf("You are %d years old\n", age);
+  // Width
+  printf("Width\n");
+  int num1 = 1;
+  int num2 = 10;
+  int num3 = -100;
 
-  int year = 2025;
-  printf("The year is %d\n", year);
+  printf("%d\n", num1);
+  printf("%+d\n", num2);
+  printf("%+d\n", num3);
+  printf("%4d\n", num1);
+  printf("%4d\n", num2);
+  printf("%4d\n", num3);
+  printf("%04d\n", num1);
+  printf("%04d\n", num2);
+  printf("%04d\n", num3);
 
-  int quantity = 2;
-  printf("You have ordered %d x items\n", quantity);
+  // Precision
+  printf("\nPrecision\n");
+  float price1 = 19.99;
+  float price2 = 1.50;
+  float price3 = -100.00;
 
-  float gpa = 2.5;
-  printf("Your GPA is %f\n", gpa);
-  printf("Your GPA is %.1f\n", gpa);
-
-  float price = 19.99;
-  printf("The price is $%.2f\n", price);
-
-  float temperature = -10.1;
-  printf("The temperature is %.1fF\n", temperature);
-
-  double pi = 3.14159265358979;
-  printf("The value of pi is %.15lf\n", pi);
-
-  double e = 2.7182818284590;
-  printf("The value of e is %.15lf\n", e);
-
-  char grade = 'A';
-  printf("Your grade is %c\n", grade);
-
-  char symbol = '!';
-  printf("Your favorite symbol is %c\n", symbol);
-
-  char currency = '$';
-  printf("The currency is %c\n", currency);
-
-  char name[] = "Imam n0ar3";
-  printf("Hello, %s\n", name);
-
-  char food[] = "pizza";
-  printf("Your favorite food is %s\n", food);
-
-  char email[] = "fake123@gmail.com";
-  printf("Your email is %s\n", email);
-
-  // true - 1 - false - 0
-  bool isOnline = true;
-  printf("%d\n", isOnline);
-  if(isOnline) {
-    printf("You are ONLINE\n");
-  }
-  else {
-    printf("You are OFFLINE\n");
-  }
-
-  // true - 1 - false - 0
-  bool isStudent = false;
-  if(isOnline) {
-    printf("You are a student\n");
-  }
-  else {
-    printf("You are not a student\n");
-  }
-
-  // true - 1 - false - 0
-  bool forSale = true;
-  if(forSale) {
-    printf("That item is for sale");
-  }
-  else {
-    printf("That item is NOT for sale");
-  }
+  printf("%.1f\n", price1);
+  printf("%.2f\n", price2);
+  printf("%.2f\n", price3);
+  printf("%7.1f\n", price1);
+  printf("%7.2f\n", price2);
+  printf("%7.2f\n", price3);
+  printf("%+7.1f\n", price1);
+  printf("%+7.2f\n", price2);
+  printf("%+7.2f\n", price3);
 
   return 0;
 }
